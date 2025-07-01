dce6aa09fb37ea039ae4010c4394d8ca20e2fa04
Muhammad Aftab Alam <143538026+Aftab0008@users.noreply.github.com>
2025-07-01 18:00:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ app.get('/', (req, res) => {
   res.send('🚀 Emergency Backend Running');
 });
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
